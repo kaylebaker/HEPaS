dbc2aff71f7df3163ddca55da4fcbf4fbc16ae33
kaylebaker <kaylebakerdev@gmail.com>
2024-04-07 21:21:17 +0800

    Moved db_interface.py functions to DB_Controller.py

---
+++ clients/db_interface.py
@@ -0,0 +1,79 @@
+import Pyro4
+import pandas as pd
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+DB_DIR = "src\server-2\oust_database.db"
+TABLES = ["Courses", "Students", "StudentUnits"]
+USER = os.getenv('DB_USERNAME')
+PASS = os.getenv('DB_PASSWORD')
+
+try:
+    # Look up server-2 in the name server
+    print("Looking up server-2 in Pyro name server...")
+    nameserver = Pyro4.locateNS()
+    uri = nameserver.lookup("server-2")
+
+    # Create a Proxy for server-2
+    server2 = Pyro4.Proxy(uri)
+    print("Server located! Now connected to server-2.")
+
+except Exception as e:
+    print("Unable to connect to remote server", e)
+
+
+print("----------------------------------")
+print("|    DATABASE INTERFACE CLIENT   |")
+print("----------------------------------\n")
+
+attempts = 3
+authorised = False
+while (attempts != 0):
+    print("Enter credentials to login.")
+    print("Username: ", end="")
+    username = input()
+    print("Password: ", end="")
+    password = input()
+
+    if username == USER and password == PASS:
+        authorised = True
+        break
+    else:
+        attempts -= 1
+        print(f"Invalid credentials. {attempts} attempts remaining.")
+
+while(authorised == True):
+    print("\nSelect from below menu options:")
+    print(" 1.\tDisplay table schema")
+    print(" 2.\tDisplay student records")
+    print(" 3.\tAdd a record")
+    print(" 4.\tDelete a record")
+    print(" 5.\tExit")
+
+    print("> ", end="")
+    selection = input()
+
+    if selection == '1':
+        for table in TABLES:
+            table_info = server2.displayDBSchema(table)
+            print(f"\nSchema details for table {table}:")
+            print(f"--------------------------------------")
+            for column in table_info:
+                print(column)
+
+    elif selection == '2':
+        data = server2.displayStudentRecords()
+        df = pd.DataFrame(data, columns = ['student_id', 'fname', 'lname', 'email', 'mobile', 'course_code', 'units_attempted', 'units_completed', 'course_status'])
+        print()
+        print(df.to_string(index=False))
+
+    elif selection == '5':
+        print("Goodbye")
+        input()
+        exit()
+
+print("Unable to login. Please contact your system administrator.")
+input()
+exit()
--- src/server-2/DB_Controller.py
+++ src/server-2/DB_Controller.py
@@ -12,10 +12,10 @@ logging.basicConfig(level=logging.DEBUG)
 
 @Pyro4.behavior(instance_mode="single")
 class Server2(object):
-
     # Set constants
     DB_DIR = ".\oust_database.db"
     VALIDATION_ERROR = "ERROR: Cannot authenticate user. Student record not found in database."
+
 
     # Function Purpose: 
     # take a tuple in format (bool, string(student_id), string(fname), string(lname), string(email))
@@ -27,12 +27,12 @@ class Server2(object):
     @Pyro4.expose
     def getStudentRecords(self, user_details):
 
-        # List to collect StudentUnit records and return from function
-        student_unit_list = []
-
         # Establish connection to SQLite database and create cursor object to perform SQL operations
         conn = sqlite3.connect(self.DB_DIR)
         cur = conn.cursor()
+
+        # List to collect StudentUnit records and return from function
+        student_unit_list = []
 
         # Check boolean value at user_details[0] to confirm if current/former student
         # Example of user_details
@@ -61,15 +61,114 @@ class Server2(object):
 
         # Use the user record to fetch all rows from table StudentUnits corresponding to that user
         query = 'SELECT student_id, unit_code, unit_score, unit_grade FROM StudentUnits WHERE student_id = ?'
-        results = cur.execute(query, (user_details[1],)).fetchall()
-
-        # Close the connection
-        conn.close()
+        results = self.cur.execute(query, (user_details[1],)).fetchall()
 
         for element in results:
             student_unit_list.append(element)
 
+        # Close connection to SQL database
+        cur.close()
+
         return student_unit_list
+
+
+    # ---------------------------------------------
+    # FUNCTIONS BELOW ARE CALLED BY db_interface.py
+    # ---------------------------------------------
+
+    @Pyro4.expose
+    def displayDBSchema(self, table_name):
+
+        # Establish connection to SQLite database and create cursor object to perform SQL operations
+        conn = sqlite3.connect(self.DB_DIR)
+        cur = conn.cursor()
+
+        table_info = []
+        schema_info = cur.execute(f"PRAGMA table_info({table_name})").fetchall()
+        for column in schema_info:
+            table_info.append(f"Column: {column[1]}, Type: {column[2]}, Nullable: {column[3]}, Default: {column[4]}")
+
+        # Close connection to SQL database
+        cur.close()
+
+        return table_info
+
+
+    @Pyro4.expose
+    def displayStudentRecords(self):
+
+        # Establish connection to SQLite database and create cursor object to perform SQL operations
+        conn = sqlite3.connect(self.DB_DIR)
+        cur = conn.cursor()
+
+        data = []
+        for record in cur.execute('SELECT * FROM Students'):
+            row_data = [record[0], record[1], record[2], record[3], record[4], record[5], record[6], record[7], record[8]]
+            data.append(row_data)
+
+        # Close connection to SQL database
+        cur.close()
+
+        return data
+
+
+    @Pyro4.expose
+    def addRecord(self, table_name, record):
+
+        # Establish connection to SQLite database and create cursor object to perform SQL operations
+        conn = sqlite3.connect(self.DB_DIR)
+        cur = conn.cursor()
+
+        try:
+
+            cur.execute(f'INSERT INTO {table_name} {record}')
+            if cur.rowcount == 1:
+                print("Record added successfully.")
+            else:
+                print("No record was inserted.")
+
+            # Close connection to SQL database
+            cur.close()
+
+        except Exception as e:
+            print("ERROR: Unable to insert new record.", e)
+
+            # Close connection to SQL database
+            cur.close()
+
+
+    @Pyro4.expose
+    def deleteRecord(self, table_name, identifier):
+
+        # Establish connection to SQLite database and create cursor object to perform SQL operations
+        conn = sqlite3.connect(self.DB_DIR)
+        cur = conn.cursor()
+
+        field = ""
+        if table_name == 'Courses':
+            field = 'course_code'
+        elif table_name == 'Students':
+            field = 'student_id'
+        else:
+            print("ERROR")
+            return
+        
+        try:
+            cur.execute(f'DELETE FROM {table_name} WHERE {field}={identifier}')
+            if cur.rowcount == 1:
+                print("Record deleted successfully.")
+            else:
+                print("No record was deleted.")
+
+            # Close connection to SQL database
+            cur.close()
+
+        except Exception as e:
+            print("ERROR: Unable to delete record.", e)
+
+            # Close connection to SQL database
+            cur.close()
+
 
 
 def main():
