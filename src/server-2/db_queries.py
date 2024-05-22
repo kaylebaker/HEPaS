097ff68501c337a8b5fe032bff7e5cdc0b39c5dd
kaylebaker <kaylebakerdev@gmail.com>
2024-05-22 12:23:44 +0800

    Deleted irrelevent files

--- src/server-2/db_queries.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-DB_DIR = "C:\\Users\\krbak\OneDrive\\Uni\\Year 3 Semester 1\\CSI3344 Distributed Systems\\Assignment 3\\Code\\HEPaS\\src\\server-2\\oust_database.db"
-
-conn = sqlite3.connect(DB_DIR)
-cur = conn.cursor()
-
-for record in cur.execute('SELECT * FROM Students'):
-    print(record)
